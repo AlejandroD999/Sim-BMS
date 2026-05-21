08cd0f273f0441e547dad809f64f0c0cbb0d81d5
AlejandroD999 <alejandrodgonzales923@gmail.com>
2026-05-21 12:46:23 -0500

    Defined class: 'AccountProcessor' to manage account

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,31 @@
-
 class Account:
-    def __init__(self):
-        self.amount = 0
-        # Account ID
-        self.acc_number = None
+    def __init__(self, acc_number, routing_number, amount=0):
+        self.amount = amount
+        self.acc_number = acc_number
         # For third party exchange
-        self.routing_number = None
-
-    def display_balance(self):
-        pass
-
-    def transfer(self):
-        pass
-
+        self.routing_number = routing_number
+    
     def deposit(self):
         pass
     
     def withdraw(self):
         pass
+
+class AccountProcessor:
+    def __init__(self, account: Account):
+        self.account = account
+    def display_balance(self):
+        return self.account.amount
+
+
+
+"""
+class TransferService:
+    def transfer(self, from_acc, to_acc, amount):
+        pass
+"""
+
+
+
+
+
