c9d4c91c7968d96486941c468233ef46ea682d1b
AlejandroD999 <alejandrodgonzales923@gmail.com>
2026-05-21 12:30:12 -0500

    Defined class: 'Account'

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+
+class Account:
+    def __init__(self):
+        self.amount = 0
+        # Account ID
+        self.acc_number = None
+        # For third party exchange
+        self.routing_number = None
+
+    def display_balance(self):
+        pass
+
+    def transfer(self):
+        pass
+
+    def deposit(self):
+        pass
+    
+    def withdraw(self):
+        pass
