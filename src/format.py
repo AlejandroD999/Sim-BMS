184851f540bd628a9083a035733ce514d5ad2ec8
AlejandroD999 <alejandrodgonzales923@gmail.com>
2026-05-23 11:55:30 -0500

    Created intro prompt

--- src/format.py
+++ src/format.py
@@ -1,1 +1,8 @@
 import rich
+import shutil
+
+def set_font_color(text: str, color: str) -> str:
+    return f"[{color}]{text}[/{color}]"
+
+sh_col_width = shutil.get_terminal_size().columns
+
--- src/prompts.py
+++ src/prompts.py
@@ -1,7 +1,10 @@
 import questionary
+from rich import print
+from format import set_font_color, sh_col_width
 
 def load_intro():
-    print("** Welcome to Sim BMS **")
+    text = set_font_color("*** Welcome to Sim BMS ***", "green")
+    print(text.center(sh_col_width))
 
 
 if __name__ == "__main__":
