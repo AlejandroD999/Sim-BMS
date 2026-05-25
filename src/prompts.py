159bd9704a1526839c0c341ccd784c68c15d33f6
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-05-25 18:53:00 -0500

    Changed menu prompt & defined account_selection function

--- src/prompts.py
+++ src/prompts.py
@@ -1,20 +1,21 @@
 import questionary
 from rich import print, prompt
-from format import set_font_color, sh_col_width
+from .format import set_font_color, sh_col_width
 
 def load_intro():
+    # Print project title
     text = set_font_color("*** Welcome to Sim BMS ***", "green")
     print(text.center(sh_col_width))
 
 def prompt_menu():
+    # Give user main menu options
     prompt_1 = questionary.select(
-        "What would you like to do",
-        ["Option 1", "Option 2", "Option 3"]).ask()
+        "Select an action for account",
+        ["Select", "Create", "Exit"]).ask()
 
     return prompt_1
 
+def account_selection():
+    # Give options of accounts
+    pass
     
-    
-if __name__ == "__main__":
-    load_intro()
-    prompt_menu()
