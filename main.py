704ed61f54e6e6db861ae95a5cdc7396e7373a8a
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-05-25 18:54:03 -0500

    Readied interface for menu functionality

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from src import utils
 from src.account import Account, AccountProcessor
+from src.prompts import load_intro, prompt_menu
 import questionary
 
+# TODO Manage accounts using SQLite3
+
+RUNNING = True
 # Interface
-while True:
-    pass
+while RUNNING:
+    load_intro()
+
+    menu_action = prompt_menu()
+
+    if menu_action.lower() == "select":
+        print(menu_action)
+
+    elif menu_action.lower() == "create":
+        print(menu_action)
+
+    else:
+        RUNNING = False
+        continue
+
