7263005d7a69800db22ac2d8c8b84c3385e4b8b4
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-05-28 09:41:09 -0500

    Defined pull_acounts

--- src/database/db.py
+++ src/database/db.py
@@ -1,16 +1,34 @@
 import sqlite3
 import os
 
-curr_dir = os.getcwd()
-with sqlite3.connect(os.path.join(curr_dir, "accounts.db")) as conn:
-    cur = conn.cursor()
+CURR_DIR = os.getcwd()
+DB_PATH = os.path.join(CURR_DIR, "accounts.db")
 
-    acc_table_query = """
-    CREATE TABLE IF NOT EXISTS accounts (
-        id INTEGER PRIMARY KEY,
-        account_number TEXT NOT NULL UNIQUE,
-        balance INTEGER NOT NULL
-        )
-        """
-    
-    cur.execute(acc_table_query)
+def create_accounts_table():
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+
+        acc_table_query = """
+        CREATE TABLE IF NOT EXISTS accounts (
+            id INTEGER PRIMARY KEY,
+            account_number TEXT NOT NULL UNIQUE,
+            balance INTEGER NOT NULL
+            )
+            """
+
+        cur.execute(acc_table_query)
+
+def pull_accounts():
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+
+        cur.execute("SELECT account_number FROM accounts")
+
+        accounts = cur.fetchall()
+
+    return accounts
+
+if __name__ == "__main__":
+    create_accounts_table()
+
+    print(pull_accounts())
