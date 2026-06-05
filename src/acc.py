a9f89eb11622779ecce89997353581cf2ad8acc0
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-06-05 10:05:55 -0500

    Enhanced error handling

--- src/acc.py
+++ src/acc.py
@@ -4,9 +4,13 @@ from .prompts import error_message
 
 class Account:
     def __init__(self, acc_number, balance=0):
-        self.acc_number = acc_number
-        self.balance = balance
+            self.acc_number: str = str(acc_number)
 
+            try:
+                self.balance: float = float(balance)
+            except ValueError:
+                error_message("Balance must be a number")
+        
     def deposit(self, amt: float):
         if 0 <= amt <= 250000:
             self.balance += amt
@@ -15,7 +19,13 @@ class Account:
         else:
             error_message("Invalid deposit amount: maximum is $250,000")
     def withdraw(self, amt):
-
+        if type(amt) != float:
+            try:
+                amt = float(amt)
+            except ValueError:
+                error_message("Amount must be a number")
+                return
+            
         if amt <= self.balance and amt > 0:
             self.balance -= amt
             db.update_balance(self.acc_number, self.balance)
@@ -34,6 +44,12 @@ class AccountProcessor:
 
 class TransferService:
     def transfer(self, from_acc, to_acc, amt):
+        try:
+            amt = float(amt)
+        except ValueError:
+            error_message("Amount must be a number")
+            return
+
         if not from_acc or not to_acc:
             error_message("A sender and a receiver must be provided")
             return
