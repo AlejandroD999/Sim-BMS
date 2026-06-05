745383d51221b76f8e8f76638f242477d286ee63
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-06-04 20:27:15 -0500

    Formatted error_messages and increased protection

--- src/acc.py
+++ src/acc.py
@@ -1,20 +1,18 @@
 import questionary as qs
 from .database import db
-
+from .prompts import error_message 
 class Account:
     def __init__(self, acc_number, balance=0):
         self.acc_number = acc_number
         self.balance = balance
-        # For third party exchange
-        #     
+
     def deposit(self, amt: float):
         if 0 <= amt <= 250000:
             self.balance += amt
             db.update_balance(self.acc_number, self.balance)
 
         else:
-            print("Invalid deposit amount: maximum is $250,000")
-    
+            error_message("Invalid deposit amount: maximum is $250,000")
     def withdraw(self, amt):
 
         if amt <= self.balance and amt > 0:
@@ -23,7 +21,7 @@ class Account:
             return True
         
         else:
-            print("Invalid withdrawal amount")
+            error_message("Invalid withdrawal amount")
             return False
         
 class AccountProcessor:
@@ -35,11 +33,11 @@ class AccountProcessor:
 
 class TransferService:
     def transfer(self, from_acc, to_acc, amt):
-        
+        if not from_acc or not to_acc:
+            error_message("A sender and a receiver must be provided")
+            return
         if from_acc.withdraw(amt):
-            to_acc.deposit(amt)
-        
-        # TODO Update database balance after transfer
+            to_acc.deposit(amt)        
 
 if __name__ == "__main__":
     acc_1 = Account(24, 54, 500)
