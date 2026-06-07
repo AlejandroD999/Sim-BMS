95a51c8a04162fc3a1f913cae445d85f109d1f01
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-06-07 17:20:40 -0500

    Changed length of acc_num

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_acc_num():
-    return random.randint(1000000, 9999999)
+    return random.randint(1000000000, 9999999999)
 
 def generate_routing_num():
-    return random.randint(1000000, 9999999)
+    return random.randint(100000000 , 999999999)
